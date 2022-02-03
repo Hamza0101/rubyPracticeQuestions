21bd6779497fbb4fb038c902f7d34bf9e4c39c74
Hamza0101 <45464935+Hamza0101@users.noreply.github.com>
2022-02-03 13:28:30 +0500

    Update q2.rb

--- q2.rb
+++ q2.rb
@@ -1,62 +1,61 @@
-class RunningSum
-    def getArray(array) 
-		i = 0
-		while (i < array.length) 
-			print("   ", array[i])
-			i += 1
-		end
-		print("\n")
-	end
-    def setArray(array,size) 
-		i = 0
-		while (i < size) 
-            print("Enter ",i,"th element of array:" )
-			array[i]=gets
-            array[i]=array[i].to_i
-			i += 1
-		end
-		print("\n")
-	end
-    def runningSum(array) 
-        i=0
-		while(i<array.length)
+# frozen_string_literal: true
 
-            if(i+1==array.length)
-                break
-            end
-            array[i+1]=array[i]+array[i+1]
-            i+=1
-        end
-	end
+class RunningSum # rubocop:disable Style/Documentation
+  def get_array(array)
+    i = 0
+    while i < array.length
+      print('   ', array[i])
+      i += 1
+    end
+    print("\n")
+  end
 
+  def set_array(array, size = 10)
+    i = 0
+    while i < size
+      print('Enter ', i, 'th element of array:')
+      array[i] = gets
+      array[i] = array[i].to_i
+      i += 1
+    end
+
+    puts
+  end
+
+  def running_sum(array)
+    i = 0
+
+    while i < array.length
+
+      break if i + 1 == array.length
+
+      array[i + 1] = array[i] + array[i + 1]
+      i += 1
+    end
+  end
 end
 
+def main
+  array = [1, 2, 3, 4, 5]
+  obj = RunningSum.new
+  puts array
+  # obj.get_array(array)
 
-
-
-
-
-def main()
-    array=[1, 2, 3, 4, 5]
-    obj=RunningSum.new()
-    
-    obj.getArray(array)
-    
-    obj.runningSum(array)
-    print("Running Sum:")
-    obj.getArray(array)
-
-
-    print("Input number of elements in array :")
-    size=0
-    size=gets
-    size=size.to_i
-    array1=Array.new
-    obj.setArray(array1,size)
-    obj.runningSum(array1)
-    print("Running Sum:")
-    obj.getArray(array1)
-
-
+  obj.running_sum(array)
+  print('Running Sum:')
+  obj.get_array(array)
 end
-main()
+
+def generic
+  print('Input number of elements in array :')
+  size = gets
+  size = size.to_i
+  array1 = []
+  obj.set_array(array1, size)
+  obj.running_sum(array1)
+  print('Running Sum:')
+  obj.get_array(array1)
+end
+
+main
+generic
