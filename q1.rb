39185b9af3e8174459a0945a961e35dabb1c0357
Hamza0101 <45464935+Hamza0101@users.noreply.github.com>
2022-02-03 13:27:13 +0500

    Update q1.rb

--- q1.rb
+++ q1.rb
@@ -1,67 +1,50 @@
+# frozen_string_literal: true
+
+# Class tally
 class Tally
-    def getArray(array) 
-		i = 0
-		while (i < array.length) 
-			print("   ", array[i])
-			i += 1
-		end
-		print("\n")
-	end
-    def setArray(array,size) 
-		i = 0
-		while (i < size) 
-            print("Enter ",i,"th element of array:" )
-			array[i]=gets
-            array[i]=array[i].to_i
-			i += 1
-		end
-		print("\n")
-	end
-    def frequency(array) 
-		#  Create a empty map
-		map = Hash.new()
-		i = 0
-		while (i < array.length) 
-			if (map.key?(array[i])) 
-				map[array[i]] = map[array[i]] + 1
-			else
- 
-				#  Add new element
-				map[array[i]] = 1
-			end
+  def get_array(array)
+    # i = 0
+    # while i < array.length
+    #   print('   ', array[i])
+    #   i += 1
+    # end
 
-			i += 1
-		end
+    # print("\n")
 
-		print("  frequency", "\n")
-		map.each { | key, value | 
-          print("  ", key ,"  =>  ", value,",")
-		}
-	end
+    array.each { |i| puts i }
+  end
 
+  def set_array(array, size)
+    i = 0
+    while i < size
+      print('Enter ', i, 'th element of array:')
+      array[i] = gets
+      array[i] = array[i].to_i
+      i += 1
+    end
+    print("\n")
+  end
+
+  def frequency(array) # rubocop:disable Metrics/MethodLength
+    #  Create a empty map
+    map = {}
+    array.each { |element| map[array[element]] = map.key?(array[element]) ? map[array[element]] + 1 : 1 }
+    puts 'Frequency: ' + map
+    puts "Frequency: #{map}"
+  end
 end
 
+def main # rubocop:disable Metrics/MethodLength
+  array = [1, 2, 3, 4, 1, 1, 1, 2, 3, 11, 11, 2, 33]
+  obj = Tally.new
+  obj.getArray(array)
+  obj.frequency(array)
 
-
-
-
-
-def main()
-    array = [1, 2, 3, 4, 1, 1, 1, 2, 3, 11, 11, 2, 33]
-    obj=Tally.new()
-    obj.getArray(array)
-    obj.frequency(array)
-
-
-
-    print("Input number of elements in array :")
-    size=0
-    size=gets
-    size=size.to_i
-    array1=Array.new
-    obj.setArray(array1,size)
-    obj.frequency(array1)
-
-
+  print('Input number of elements in array :')
+  size = gets
+  size = size.to_i
+  array1 = []
+  obj.setArray(array1, size)
+  obj.frequency(array1)
 end
-main()
+main
